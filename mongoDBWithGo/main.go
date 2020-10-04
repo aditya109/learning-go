4d51b4aadbf2f7da94886cf75fa21a96ce5e0fdd
Aditya <adikid1996@gmail.com>
2020-10-04 22:23:47 +0530

    fixed gitguardian policy break (#3)
    Signed-off-by: aditya109 <adikid1996@gmail.com>

--- mongoDBWithGo/main.go
+++ mongoDBWithGo/main.go
@@ -11,13 +11,13 @@ import (
 
 func main() {
 
-
+	URL := "Mongo URL"
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(
-		"mongodb+srv://daityaADMs:9Nmfbp6l1MAz@cluster0.7tvor.mongodb.net/<dbname>?retryWrites=true&w=majority",
+		URL,
 	))
-	fmt.Println()
+	fmt.Println(client)
 	if err != nil { log.Fatal(err) }
 
 
